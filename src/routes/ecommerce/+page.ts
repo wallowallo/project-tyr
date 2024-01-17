87b31439ed6d23bd81d2ccf035b5ccb216fbac42
Jan Aage Lavik <jan.aage.lavik@bidbax.no>
2024-01-17 10:51:43 +0100

    Load products from JSON file in frontend app

--- src/routes/ecommerce/+page.ts
+++ src/routes/ecommerce/+page.ts
@@ -1,12 +1,10 @@
 import type { Product } from "$lib/types";
+import products from "data/products.json";
 
+/**
+ * Load products from JSON file.
+ * Exported in +page.svelte as data.products
+ */
 export function load() {
-	return {
-		products: [
-			{ id: '0001', name: 'Rolex Watch', price: 1000.00 },
-            { id: '0002', name: 'Michael Kors Purse', price: 80.00 },
-			{ id: '0003', name: 'iPhone XS', price: 400.00 },
-            { id: '0004', name: 'Casio Watch', price: 30.00 },
-		] as Product[]
-	};
+	return { products };
 }
